e212e921bb0cde2fd22835c60c47e8179507de8e
ishalvss <isha.lanjwal@vishleshan.net>
2023-05-15 15:02:27 +0530

    setter getter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'getter-setter';
+  private _customerName: string = ""
+  ngOnInit(): void {
+    
+  }
+
+  get customerName(){
+    return this._customerName;
+  }
+
+  set customerName(value: string){
+    this._customerName = value;
+    if(value == "Hello World"){
+      alert("Hello World")
+    }
+  }
 }
