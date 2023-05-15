1a5d55928fad1185c4826293cd7c9a60bcf4296c
isha-lanjwal <111967698+isha-lanjwal@users.noreply.github.com>
2023-05-15 15:09:10 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import 'Zone.js';
+import 'zone.js';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
